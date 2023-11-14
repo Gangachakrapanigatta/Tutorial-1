643b5b97534e11a597fd15f2ae110d788be5a9eb
Gangachakrapanigatta <143632786+Gangachakrapanigatta@users.noreply.github.com>
2023-11-14 22:59:04 +0530

    Add files via upload

---
+++ Untitled13.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main()
+{
+char g;
+printf("enter the choice of char");
+scanf("%c",&g);
+   switch(g)
+   {
+   	 case 'a':
+   	 	printf("you enter choice: a");
+   	 	 break;
+   	 case 'b':
+   	 	printf("you enter choice: b");
+   	  	break;
+   	 case 'c':
+   	 	printf("you enter choice: c");	
+   	  break;	
+   	 default:
+		printf("you entered choice is not there");	
+     }
+	
+}
+
+
+
+
+
+
+
+
